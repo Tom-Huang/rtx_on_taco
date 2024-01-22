a50e0326fd6ce5993dd33958b80188b6d42bf491
Tom-Huang <huang@informatik.uni-freiburg.de>
2024-01-22 19:37:04 +0100

    new inference code

--- hulc2/rollout/real_world_rollout_orca.py
+++ hulc2/rollout/real_world_rollout_orca.py
@@ -22,12 +22,65 @@ from robot_io.utils.utils import quat_to_euler
 # from calvin_env.utils.utils import angle_between_angles
 from hulc2.wrappers.panda_rtx_wrapper import PandaRTXWrapper
 from hulc2.evaluation.utils import imshow_tensor
-from orca.utils.pretrained_utils import ORCAModel
+from octo.model.octo_model import OctoModel
+
+N_DIGITS = 7
+MAX_REL_POS = 0.02
+MAX_REL_ORN = 0.05
+
+
+def get_ep_start_end_ids(path):
+    return np.sort(np.load(Path(path) / "ep_start_end_ids.npy"), axis=0)
+
+
+def get_frame(path, i):
+    filename = Path(path) / f"episode_{i:0{N_DIGITS}d}.npz"
+    return np.load(filename, allow_pickle=True)
+
+
+def reset(env, path, i):
+    data = get_frame(path, i)
+    robot_state = data["robot_obs"]
+    gripper_state = "open" if robot_state[6] > 0.07 else "closed"
+    env.reset(
+        target_pos=robot_state[:3],
+        target_orn=robot_state[3:6],
+        gripper_state=gripper_state,
+    )
+
+
+def get_action(path, i, use_rel_actions=False, control_frame="tcp"):
+    frame = get_frame(path, i)
+    if control_frame == "tcp":
+        action = frame["rel_actions_gripper"]
+    else:
+        print("come here")
+        action = frame["rel_actions_world"]
+    print(action)
+    # pos = action[:3] * MAX_REL_POS
+    # orn = action[3:6] * MAX_REL_ORN
+
+    # new_action = {"motion": (pos, orn, action[-1]), "ref": "rel"}
+    # return new_action
+    return action
+
+
+def reset(env, path, i):
+    data = get_frame(path, i)
+    robot_state = data["robot_obs"]
+    gripper_state = "open" if robot_state[6] > 0.07 else "closed"
+    env.reset(
+        target_pos=robot_state[:3],
+        target_orn=robot_state[3:6],
+        gripper_state=gripper_state,
+    )
 
 
 def load_model(cfg):
-    model = ORCAModel.load_pretrained(cfg.checkpoint_dir)
-    statistics = statistics = ORCAModel.load_dataset_statistics(cfg.checkpoint_dir, "taco_play")
+    # model = ORCAModel.load_pretrained(cfg.checkpoint_dir)
+    # statistics = statistics = ORCAModel.load_dataset_statistics(cfg.checkpoint_dir, "taco_play")
+    model = OctoModel.load_pretrained(cfg.checkpoint_dir)
+    statistics = model.dataset_statistics
 
     return model, statistics
 
@@ -42,10 +95,16 @@ def lang_rollout(model, env, statistics, goal, ep_len=500, pred_horizon=1, exp_w
             pred_horizon=pred_horizon, exp_weight=exp_weight, chunk=chunk)
 
 
-def image_rollout(model, env, statistics, ep_len=500, pred_horizon=1, exp_weight=1, chunk=0):
-    goal_obs = env.get_obs()
+def image_rollout(model, env, statistics, goal_obs=None, ep_len=500, pred_horizon=1, exp_weight=1, chunk=0):
+    if goal_obs is None:
+        goal_obs = env.get_obs()
     goal_image = goal_obs["rgb_static"]
     goal_image_wrist = goal_obs["rgb_gripper"]
+    goal_image_bgr = cv2.cvtColor(goal_image, cv2.COLOR_RGB2BGR)
+    goal_image_wrist_bgr = cv2.cvtColor(goal_image_wrist, cv2.COLOR_RGB2BGR)
+    cv2.imshow("goal_image", goal_image_bgr)
+    cv2.imshow("goal_image_gripper", goal_image_wrist_bgr)
+    cv2.waitKey()
 
     goal_image = cv2.resize(goal_image, (256, 256))
     goal_image_wrist = cv2.resize(goal_image_wrist, (128, 128))
@@ -57,6 +116,7 @@ def image_rollout(model, env, statistics, ep_len=500, pred_horizon=1, exp_weight
     goal_image = goal_image[None, ...]
     goal_image_wrist = goal_image_wrist[None, ...]
     task_image = model.create_tasks(goals={"image_0": goal_image, "image_1": goal_image_wrist})
+    # task_image = model.create_tasks(goals={"image_0": goal_image})
     policy_fn = jax.jit(model.sample_actions)
     rollout(env, policy_fn, task_image, "image goal", statistics,
             ep_len=ep_len, pred_horizon=pred_horizon, exp_weight=exp_weight, chunk=chunk)
@@ -67,13 +127,13 @@ def rollout(env, policy_fn, task, goal, statistics, horizon=2, ep_len=5000, pred
     observations = {
         "image_0": np.zeros((1, horizon, 256, 256, 3), dtype=np.uint8),  # batch, horizon, width, height, channels,
         "image_1": np.zeros((1, horizon, 128, 128, 3), dtype=np.uint8),  # for wrist camera
-        "pad_mask": np.array([[True, True]])
+        "timestep_pad_mask": np.array([[True, True]])
     }
     obs_deque = collections.deque(maxlen=horizon)
     obs_deque_wrist = collections.deque(maxlen=horizon)
     # env.reset()
     for i in range(20):
-        act = torch.from_numpy(np.array([0, -0.5, 0, 0, 0, 0, 0]))
+        act = torch.from_numpy(np.array([0, 0, 0., 0, 0, 0, 0]))
         env.step(act)
     # goal_embed = embed_model([goal])[0]
     obs = env.get_obs()
@@ -116,10 +176,8 @@ def rollout(env, policy_fn, task, goal, statistics, horizon=2, ep_len=5000, pred
             pred_action_np_chunk = pred_action_np.copy()
             pred_action_np = ewa_action.get_action(pred_action_np)
             pred_action_torch = torch.from_numpy(pred_action_np)
-            print("torch: ", pred_actions)
             # transform the gripper action from [0, 1] to [-1, 1]
             # pred_action_torch[0][-1] = pred_action_torch[0][-1] * 2. - 1.
-            print("change: ", pred_actions)
             if chunk_step_count < chunk:
                 chunk_step_count += 1
         else:
@@ -188,17 +246,23 @@ def main(cfg):
     model, statistics = load_model(cfg)
     env = PandaRTXWrapper(env, relative_action=True)
 
-    # goal = "move the slider left"
-    goal = "turn on the green light"
-    # goal = "turn on the blue light"
-    # goal = "turn on the red light"
-    # goal = "move the slider right"
-    # goal = "stack the blue block on the green block"
-    # goal = "unstack the blue block"
-    # goal = "open the drawer"
+    ep_start_end_ids = get_ep_start_end_ids(cfg.load_dir)
+    for start_idx, end_idx in ep_start_end_ids:
+        # reset(env, cfg.load_dir, start_idx)
+        target_obs = get_frame(cfg.load_dir, end_idx)
+        # image_rollout(model, env, statistics, goal_obs=target_obs, ep_len=500, pred_horizon=1, exp_weight=1, chunk=1)
 
-    # lang_rollout(model, env, statistics, goal, ep_len=500, pred_horizon=1, exp_weight=1, chunk=4)
-    image_rollout(model, env, statistics, ep_len=500, pred_horizon=1, exp_weight=1, chunk=4)
+        # goal = "move the slider left"
+        goal = "turn on the green light"
+        # goal = "turn on the blue light"
+        # goal = "turn on the red light"
+        # goal = "move the slider right"
+        # goal = "stack the blue block on the green block"
+        # goal = "unstack the blue block"
+        # goal = "open the drawer"
+
+        lang_rollout(model, env, statistics, goal, ep_len=500, pred_horizon=4, exp_weight=1, chunk=1)
+        # image_rollout(model, env, statistics, ep_len=500, pred_horizon=1, exp_weight=1, chunk=4)
 
 
 if __name__ == "__main__":
